3dce1116757d2854e19296c731880ad6106b3d40
ytyaru <ytyaru@outlook.jp>
2021-02-01 12:24:19 +0900

    ボタンのコードを別ファイル化

---
+++ docs/7/CodeTag.js
@@ -0,0 +1,14 @@
+class CodeTag {
+    static parse() {
+        const CLASS_COPY_BUTTON = 'code-copy-btn';
+        const codes = document.querySelectorAll('code[src]');
+        codes.forEach((code, index) => {
+            CodeViewer.create(code, index, CLASS_COPY_BUTTON);
+        });
+        const clipboard = new ClipboardJS(`.${CLASS_COPY_BUTTON}`);
+        clipboard.on('success', (e) => {
+            document.getSelection().empty();
+        });
+    }
+}
+
